57c4dbf45dd17fc745c0ca7eb1f117a5f14c088b
Rob Harrigan <rharrigan@50onred.com>
2013-10-05 00:17:26 -0400

    Draft of prefernces

--- android/wandersafe/WanderSafe/src/main/java/com/fifty/wdrsfe/Main.java
+++ android/wandersafe/WanderSafe/src/main/java/com/fifty/wdrsfe/Main.java
@@ -10,14 +10,31 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        addPreferencesFromResource(R.xml.preferences);
     }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        menu.add(Menu.NONE, 0, 0, "Show current settings");
+        return super.onCreateOptionsMenu(menu);
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                startActivity(new Intent(this, ShowSettingsActivity.class));
+                return true;
+        }
+        return false;
+    }
 }
