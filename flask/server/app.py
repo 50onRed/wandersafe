3b9c0073852b9232f03c75a08658914d479d8884
Ivan Dyedov <idyedov@50onred.com>
2013-10-05 15:07:24 -0400

    change routes

--- flask/server/app.py
+++ flask/server/app.py
@@ -4,6 +4,6 @@ from .views import tell_me_if_im_going_to_die, show_map
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config_default.py')
-    app.add_url_rule('/', 'tell_me_if_im_going_to_die', tell_me_if_im_going_to_die)
-    app.add_url_rule('/map', 'show_map', show_map)
+    app.add_url_rule('/level/<lat>/<lon>/<meters>', 'tell_me_if_im_going_to_die', tell_me_if_im_going_to_die)
+    app.add_url_rule('/map/<lat>/<lon>/<meters>', 'show_map', show_map)
     return app
--- flask/server/views.py
+++ flask/server/views.py
@@ -1,19 +1,16 @@
 from flask import request, render_template
 from .stackmob import get_crime_near
 
-
-def tell_me_if_im_going_to_die():
-    response = get_crime_near(float(request.args['latitude']),
-                              float(request.args['longitude']),
-                              float(request.args['meters']))
+def tell_me_if_im_going_to_die(lat, lon, meters):
+    lat, lon, meters = float(lat), float(lon), float(meters)
+    response = get_crime_near(lat, lon, meters)
     level = process_crime_level(response)
     return str(level)
 
-def show_map():
-    lat = float(request.args['latitude'])
-    long = float(request.args['longitude'])
-    response = get_crime_near(lat, long, float(request.args['meters']))
-    return render_template("map.html", lat=lat, long=long, crimes=response)
+def show_map(lat, lon, meters):
+    lat, lon, meters = float(lat), float(lon), float(meters)
+    response = get_crime_near(lat, lon, meters)
+    return render_template("map.html", lat=lat, long=lon, crimes=response)
 
 def process_crime_level(crimes):
     # This level is not correct
