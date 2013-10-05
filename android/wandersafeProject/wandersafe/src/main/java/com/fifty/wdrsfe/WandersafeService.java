520116ffa509ff0c9769c90635a0566761b296c1
Steve Dorazio <sdorazio@50onred.com>
2013-10-04 23:10:51 -0400

    Adding some service changes

--- android/wandersafeProject/wandersafe/src/main/java/com/fifty/wdrsfe/WandersafeService.java
+++ android/wandersafeProject/wandersafe/src/main/java/com/fifty/wdrsfe/WandersafeService.java
@@ -9,8 +9,28 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class WandersafeService extends Service implements LocationListener {
+
+    private final static String TAG = "WandersafeService";
+
+    private final static String BASE_URL = "";
 
     private LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -57,7 +77,61 @@ public class WandersafeService extends Service implements LocationListener {
         //Not Implemented
     }
 
-    private void createNotification(){
+    private void createNotification() {
 
+    }
+
+    /**
+     * Gets the alert level based on the geo location and the radius
+     * @param latitude
+     * @param longitude
+     * @param radius
+     * @return
+     */
+    private int getAlertLevel(double latitude, double longitude, int radius) {
+        int level = -1;
+
+        try {
+            JSONObject jsonObject = getJSONFromURL(BASE_URL + "?latitude="+latitude+"&longitude="+longitude+"&radius="+radius);
+            level = jsonObject.getInt("level");
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to retrieve data", e);
+        } catch (JSONException e) {
+            Log.e(TAG, "Unable to retrieve data", e);
+        }
+
+        return level;
+    }
+
+    public static JSONObject getJSONFromURL(String url) throws ClientProtocolException, IOException, JSONException {
+        JSONObject responseJSON = null;
+        InputStream in = null;
+
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(url);
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            in = httpResponse.getEntity().getContent();
+
+            //If invalid response type, throw exception
+            if(httpResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                throw new IOException("Unable to get JSON from URL: "+url);
+            }
+
+            StringBuilder response = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            //Parse the response
+            responseJSON = (JSONObject) new JSONTokener(response.toString()).nextValue();
+        }
+        finally {
+            if(in != null) { in.close(); }
+        }
+
+        return responseJSON;
     }
 }
