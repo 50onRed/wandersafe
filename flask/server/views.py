e4c7670fdf7e9a377f9bd5032753ff474702ac20
Ivan Dyedov <idyedov@50onred.com>
2013-10-05 15:28:25 -0400

    s/long/lon to be consistent

--- flask/server/views.py
+++ flask/server/views.py
@@ -13,7 +13,7 @@ def tell_me_if_im_going_to_die(lat, lon, meters):
 def show_map(lat, lon, meters):
     lat, lon, meters = float(lat), float(lon), float(meters)
     response = get_crime_near(lat, lon, meters)
-    return render_template("map.html", lat=lat, long=lon, crimes=response)
+    return render_template("map.html", lat=lat, lon=lon, crimes=response)
 
 def process_crime_level(crimes):
     # This level is not correct
