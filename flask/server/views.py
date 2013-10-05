fd6797cd1eb061e8f7fc146bd2310589758d8ff1
Matt <mschmoyer@50onred.com>
2013-10-04 23:22:12 -0400

    One view so no need for blueprints

--- flask/server/views.py
+++ flask/server/views.py
@@ -1,7 +1,2 @@
-from flask import Blueprint
-
-wandersafe = Blueprint('wandersafe', __name__)
-
-@wandersafe.route('/tell_me_if_im_going_to_die')
 def tell_me_if_im_going_to_die():
     return 'Probably'
