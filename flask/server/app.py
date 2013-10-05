e322e504741ed00a60f56e666ea0e077d9da77f3
Ivan Dyedov <idyedov@50onred.com>
2013-10-05 13:08:09 -0400

    add basic map route

--- flask/server/app.py
+++ flask/server/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from .views import tell_me_if_im_going_to_die
+from .views import tell_me_if_im_going_to_die, show_map
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config_default.py')
     app.add_url_rule('/', 'tell_me_if_im_going_to_die', tell_me_if_im_going_to_die)
+    app.add_url_rule('/map', 'show_map', show_map)
     return app
--- flask/server/views.py
+++ flask/server/views.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, render_template
 from .stackmob import get_crime_near
 
 
@@ -7,12 +7,18 @@ def tell_me_if_im_going_to_die():
                               float(request.args['longitude']),
                               float(request.args['meters']))
     level = process_crime_level(response)
+    return str(level)
+
+def show_map():
+    lat = float(request.args['latitude'])
+    long = float(request.args['longitude'])
+    response = get_crime_near(lat, long, float(request.args['meters']))
+    return render_template("map.html", lat=lat, long=long, crimes=response)
+
+def process_crime_level(crimes):
     # This level is not correct
     # ucr_general is the crime level code, not the num of occurences
     # TODO: Fix the method of calculating a level
-    return str(level)
-
-def process_crime_level(crimes):
     if len(crimes) == 0:
         return 0 # avoid division by zero
     total = sum([int(crime['ucr_general']) / 100 for crime in crimes])
