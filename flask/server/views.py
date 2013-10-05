df925808abc99b70f829e50542e148cd133db6cb
Matt <mschmoyer@50onred.com>
2013-10-05 01:43:45 -0400

    Returns level

--- flask/server/views.py
+++ flask/server/views.py
@@ -3,7 +3,18 @@ from .stackmob import get_crime_near
 
 
 def tell_me_if_im_going_to_die():
-    status= get_crime_near(float(request.args['latitude']),
-                           float(request.args['longitude']),
-                           float(request.args['meters']))
-    return '{}'.format(status)
+    response = get_crime_near(float(request.args['latitude']),
+                              float(request.args['longitude']),
+                              float(request.args['meters']))
+    level = process_crime_level(response)
+    # This level is not correct
+    # ucr_general is the crime level code, not the num of occurences
+    # TODO: Fix the method of calculating a level
+    return str(level)
+
+def process_crime_level(crimes):
+    total = 0
+    for crime in crimes:
+        total += int(crime['ucr_general'])/100
+    return total/len(crimes)
+        
