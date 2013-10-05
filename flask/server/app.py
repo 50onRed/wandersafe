ff399b87019449ca2b8e2f95f075502e478c5441
Matt <mschmoyer@50onred.com>
2013-10-05 00:15:29 -0400

    Adds reversed lat and long

--- flask/server/app.py
+++ flask/server/app.py
@@ -6,7 +6,3 @@ def create_app():
     app.config.from_pyfile('config_default.py')
     app.add_url_rule('/', 'tell_me_if_im_going_to_die', tell_me_if_im_going_to_die)
     return app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run()
--- flask/server/views.py
+++ flask/server/views.py
@@ -1,7 +1,12 @@
 from flask import request
 from .stackmob import get_crime_near
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 def tell_me_if_im_going_to_die():
-    return get_crime_near(request.args['latitude'],
-                          request.args['longitude'],
-                          request.args['radius'])
+    status= get_crime_near(float(request.args['longitude']),
+                           float(request.args['latitude']),
+                           float(request.args['meters']))
+    logging.debug(status)
+    return '{}'.format(status)
