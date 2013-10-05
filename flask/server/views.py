c210a0390ba8becd7a9a7e271d16500bb3a9948e
Matt <mschmoyer@50onred.com>
2013-10-04 23:42:01 -0400

    Adds get_crime_status to view

--- flask/server/views.py
+++ flask/server/views.py
@@ -1,2 +1,7 @@
+from flask import request
+from .stackmob import get_crime_near
+
 def tell_me_if_im_going_to_die():
-    return 'Probably'
+    return get_crime_near(request.args['latitude'],
+                          request.args['longitude'],
+                          request.args['radius'])
