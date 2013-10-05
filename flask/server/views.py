3818858489de6b76522dc19f400e2b7ce020976d
Matt <mschmoyer@50onred.com>
2013-10-05 00:17:05 -0400

    unreversed

--- flask/server/views.py
+++ flask/server/views.py
@@ -1,12 +1,9 @@
 from flask import request
 from .stackmob import get_crime_near
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
 
 def tell_me_if_im_going_to_die():
-    status= get_crime_near(float(request.args['longitude']),
-                           float(request.args['latitude']),
+    status= get_crime_near(float(request.args['latitude']),
+                           float(request.args['longitude']),
                            float(request.args['meters']))
-    logging.debug(status)
     return '{}'.format(status)
