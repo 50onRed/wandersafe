0f612e7ac779dd8ae217664d2483ebec59655958
Matt <mschmoyer@50onred.com>
2013-10-05 20:11:35 -0400

    Hold off on the weighting for now

--- flask/server/views.py
+++ flask/server/views.py
@@ -1,6 +1,9 @@
 from flask import request, render_template
 from .stackmob import get_crime_near
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 def index():
     return render_template("index.html")
 
@@ -16,12 +19,14 @@ def show_map(lat, lon, meters):
     return render_template("map.html", lat=lat, lon=lon, crimes=response)
 
 def process_crime_level(crimes):
+    # logging.debug(crimes)
     if len(crimes) == 0:
         return 0 # avoid division by zero
     sum_of_distances = sum([float(crime['location']['distance']) for crime in crimes])
-    total = sum([weight_crime_level(crime, sum_of_distances, len(crimes)) for crime in crimes])
-    return 8 - int(total*len(crimes))/1000
+    total = sum([float(crime['ucr_general']) for crime in crimes])
+    return 8 - int(total / 100.00 / len(crimes))
 
 def weight_crime_level(crime, sum_of_distances, num_of_crimes):
     distance = float(crime['location']['distance'])
+    logging.debug(type(distance))
     return (float(crime['ucr_general'])/num_of_crimes)*((sum_of_distances - distance)/sum_of_distances)
