df7c30bb612844a227c872b7d71dfc0ae5b3f38b
Matt <mschmoyer@50onred.com>
2013-10-06 13:34:35 -0400

    Updates weighted distance as sum - current

--- flask/server/views.py
+++ flask/server/views.py
@@ -1,6 +1,6 @@
+from pprint import pprint
 from flask import request, render_template
 from .stackmob import get_crime_near
-
 
 def index():
     return render_template("index.html")
@@ -17,12 +17,14 @@ def show_map(lat, lon, meters):
     return render_template("map.html", lat=lat, lon=lon, crimes=response)
 
 def process_crime_level(crimes):
+    pprint(crimes)
     if len(crimes) == 0:
         return 0 # avoid division by zero
     sum_of_distances = sum([float(crime['location']['distance']) for crime in crimes])
     total = sum([weight_crime_level(crime, sum_of_distances) for crime in crimes])
-    return 8 - int(total / 100.00/ sum_of_distances)
+    new_sum_of_distances = sum([sum_of_distances - float(crime['location']['distance']) for crime in crimes])
+    return 8 - int(total / 100.0 / new_sum_of_distances)
 
 def weight_crime_level(crime, sum_of_distances):
     distance = float(crime['location']['distance'])
-    return float(crime['ucr_general'])*distance
+    return float(crime['ucr_general']) * (sum_of_distances - distance)
