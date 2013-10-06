897915161c2f2e28c3b79da4b4bb358ebe9ac93d
Matt <mschmoyer@50onred.com>
2013-10-06 12:57:37 -0400

    Adds weighted arithmetic mean

--- flask/server/views.py
+++ flask/server/views.py
@@ -1,8 +1,6 @@
 from flask import request, render_template
 from .stackmob import get_crime_near
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
 
 def index():
     return render_template("index.html")
@@ -19,14 +17,12 @@ def show_map(lat, lon, meters):
     return render_template("map.html", lat=lat, lon=lon, crimes=response)
 
 def process_crime_level(crimes):
-    # logging.debug(crimes)
     if len(crimes) == 0:
         return 0 # avoid division by zero
     sum_of_distances = sum([float(crime['location']['distance']) for crime in crimes])
-    total = sum([float(crime['ucr_general']) for crime in crimes])
-    return 8 - int(total / 100.00 / len(crimes))
+    total = sum([weight_crime_level(crime, sum_of_distances) for crime in crimes])
+    return 8 - int(total / 100.00/ sum_of_distances)
 
-def weight_crime_level(crime, sum_of_distances, num_of_crimes):
+def weight_crime_level(crime, sum_of_distances):
     distance = float(crime['location']['distance'])
-    logging.debug(type(distance))
-    return (float(crime['ucr_general'])/num_of_crimes)*((sum_of_distances - distance)/sum_of_distances)
+    return float(crime['ucr_general'])*distance
